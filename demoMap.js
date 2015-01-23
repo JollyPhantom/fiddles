599663a77590d18b0d55e5bb32f84a74650556c8
Devin Otway <devin.otway@gmail.com>
2015-01-22 23:39:38 -0800

    creates map that conforms to demoMap

--- demoMap.js
+++ demoMap.js
@@ -1,35 +1,19 @@
 $(document).ready(function(){
 
-  // these are the shelves used in the demoMap, however, while this is hardcoded now
-  // we will need to actuall retrieve these on the fly from the DB
-  // every vendor will have a unique ID that specifies their map, shelves, items, etc.
-  var shelvesFt = [];
-  shelvesFt[0] = [5,0,10,1];
-  shelvesFt[1] = [0,9,1,18];
-  shelvesFt[2] = [19,0,1,36];
-  shelvesFt[3] = [5,17,10,1];
-  shelvesFt[4] = [5,19,10,1];
-  shelvesFt[5] = [5,35,10,1];
-
-  // this will contain the shelves in SVG/Pixel format
-  var shelves = [];
- 
-  // in feet;
-  var roomWidth = 20;
   var roomHeight = 36;
-
+  var roomWidth = 20;
   var width = $(document).width();
   var height = $(document).height();
-  var scale = roomWidth;
+  var scale = Math.max(height/roomHeight, width/roomWidth);
 
   var feetToPixel = function(ft){
     var foot = width/scale;
     return ft*foot;
   }
 
-  // coors is in array
   var coorsToString = function(coors, convertToPixel){
     var result = '';
+
     for(var i = 0; i < coors.length; i++){
       var x = coors[i][0];
       var y = coors[i][1];
@@ -39,36 +23,16 @@ $(document).ready(function(){
       }
       result = result+x+','+y+' ';
     }
+
     return result;
   }
 
-  // converts the cartesian coordinates (origin at bottom left) to coordinates for svg (origin at top left)
-  var cartesianToSVGCoords = function(coords) {
-    // coords is an array in the format of [x,y,w,h];
-    // this function utilizes the variables roomHeight and roomWidth in order figure out the SVG coordinates
-    // y = roomHeight - h;
-    var _coords = coords.slice();
-    _coords[1] = roomHeight - coords[3];
-    // what is returned is not actual in the pixel coordiantes, but the x,y where the origin is the top left corner of the room
-    return _coords;
-  };
-
-  // creates an shelf object with x,y coords and width and height in pixels
   var createShelves = function(x,y,w,h){
-    if ( typeof arguments[0] === 'number' ) {
-      return {
-        x:feetToPixel(x),
-        y:feetToPixel(y),
-        width:feetToPixel(w),
-        height:feetToPixel(h)
-      };
-    } else if ( Array.isArray(arguments[0]) ) {
-      return {
-        x: feetToPixel(arguments[0][0]),
-        y: feetToPixel(arguments[0][1]),
-        width: feetToPixel(arguments[0][2]),
-        height: feetToPixel(arguments[0][3])
-      };
+    return {
+      x:feetToPixel(x),
+      y:feetToPixel(y),
+      width:feetToPixel(w),
+      height:feetToPixel(h)
     }
   }
 
@@ -81,24 +45,22 @@ $(document).ready(function(){
   var floorplan = svg.append('polygon')
                     .attr('points',coorsToString([
                       [0,0],
-                      [0,36],
+                      [20,0],
                       [20,36],
-                      [36,0]
+                      [0,36]
                       ],true))
-                    .attr('fill','white');
-                    // .attr('stroke','blue');
+                    .attr('fill','white')
+                    .attr('stroke','blue');
+
+  var shelf1 = createShelves(5,0,15,1);
+  var shelf2 = createShelves(19,0,1,36);
+  var shelf3 = createShelves(5,35,15,1);
+  var shelf4 = createShelves(0,9,1,18);
+  var shelf5 = createShelves(5,17,10,1);
+  var shelf6 = createShelves(5,18,10,1);
 
 
-  // creates an array of 'shelves' that have the coords in pixels/svg format
-  shelvesFt.forEach(function(shelf, index){
-    console.log(shelf);
-    shelves.push(createShelves(shelf));
-  });
-
-  console.log(shelves);
-  // var shelf1  = createShelves(1,1,1,8);
-  // var shelf2  = createShelves(8,1,1,4);
-  // var shelves = [shelf1,shelf2];
+  var shelves = [shelf1, shelf2, shelf3, shelf4, shelf5, shelf6];
 
   svg.selectAll('rect').data(shelves)
     .enter().append('rect')
@@ -106,6 +68,9 @@ $(document).ready(function(){
     .attr('y',function(d){return d.y})
     .attr('width',function(d){return d.width})
     .attr('height',function(d){return d.height})
-    .attr('stroke','red')
+    .attr('stroke','black')
     .attr('fill','red')
+
+
+
 });
